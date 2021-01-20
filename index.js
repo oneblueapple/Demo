7c2fe14994ef9be3c378ca413286ae3694173777
oneblueapple <63837554+oneblueapple@users.noreply.github.com>
2021-01-20 11:16:19 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_auth_key_and_cert.htm
+
+https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_auth_connected_app.htm
+
+https://help.salesforce.com/articleView?id=remoteaccess_oauth_jwt_flow.htm&type=5
+
+
+https://blogs.oracle.com/blogbypuneeth/steps-to-create-a-jks-keystore-using-key-and-crt-files
+
+https://sourceforge.net/projects/openssl/
+https://www.atmarkit.co.jp/ait/articles/1601/29/news043.html
